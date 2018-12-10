02bfa4732b5a435abe7af82d02b0e7a6e12e9c5a
kozimon0204 <kozimon0204@gmail.com>
2018-12-10 22:34:48 +0900

    add devise, route

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable,  and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         # Add module
+         :trackable
+
+
+  def update_without_password(params, *options)
+    # current_password = params.delete(:current_password)
+
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = if valid_password?(current_password)
+      update_attributes(params, *options)
+    else
+        self.assign_attributes(params, *options)
+        self.valid?
+        self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+        false
+    end
+
+    update_attributes(params, *options)
+
+    clean_up_passwords
+    result
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  get 'users', to: 'users#index', as: :users
+  
+  # devise_for :users
   root 'home#index'
   get 'home/index'
+
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
+  devise_scope :user do
+    get 'logout' => 'devise/sessions#destroy'
+    get 'users/:id/edit' => 'users/registrations#edit', as: :edit_other_user_registration
+    match 'users/:id', to: 'users/registrations#update', via: [:patch, :put], as: :other_user_registration
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
