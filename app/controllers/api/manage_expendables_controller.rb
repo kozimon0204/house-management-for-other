be51c151e9d27f2773b6e6317d45058837d9db82
kozimon0204 <kozimon0204@gmail.com>
2018-12-15 12:47:06 +0900

    add API, send grid

--- app/controllers/api/manage_expendables_controller.rb
+++ app/controllers/api/manage_expendables_controller.rb
@@ -7,7 +7,7 @@ module Api
     # ログイン情報からユーザーIDを取得し、
     # 紐づく注文履歴一覧を取得
     # 
-    # [GET] /api/v1/app/slips
+    # [POST] /api/manage_expendables/add
     # 
     def add
       expendableChoice = ExpendableChoice.new(expendable_params)
@@ -18,9 +18,39 @@ module Api
       render json: '', status: 204
     end
 
+    # 
+    # 足す
+    # 
+    # [PUT] /api/manage_expendables/plus
+    # 
+    def plus
+      expendableChoice = ExpendableChoice.find(expendable_choice_params[:id])
+      expendableChoice.amount += 1
+      expendableChoice.save!
+
+      render json: '', status: 204
+    end
+
+    # 
+    # 引く
+    # 
+    # [PUT] /api/manage_expendables/minus
+    # 
+    def minus
+      expendableChoice = ExpendableChoice.find(expendable_choice_params[:id])
+      expendableChoice.amount -= 1
+      expendableChoice.save!
+
+      render json: '', status: 204
+    end
+
     private
     def expendable_params
       params.require(:expendable).permit(:house_id, :expendable_id)
+    end
+
+    def expendable_choice_params
+      params.require(:expendable_choice).permit(:id)
     end
 
   end
--- config/routes_api.rb
+++ config/routes_api.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     post 'manage_expendables/add', to: 'manage_expendables#add'
-    
+    put 'manage_expendables/plus', to: 'manage_expendables#plus'
+    put 'manage_expendables/minus', to: 'manage_expendables#minus'
   end
   
 end
