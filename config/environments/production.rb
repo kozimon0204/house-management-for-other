35cabf14d0a855587f8dd35e2d8b705a207d69fc
kozimon0204 <kozimon0204@gmail.com>
2018-12-15 13:16:24 +0900

    fixup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method       = :stmp
+  config.action_mailer.delivery_method       = :smtp
   config.action_mailer.default_url_options   = { host: 'house-zoo-management.herokuapp.com' }
   ActionMailer::Base.smtp_settings           = {
       address:              'smtp.sendgrid.net',
